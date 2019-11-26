ad4e1c5f7bb58a9bd89612872439ab579cfa9c62
kkaruso <56772447+kkaruso@users.noreply.github.com>
2019-11-26 23:45:50 +0100

    Add files via upload

---
+++ FitnessPal/Class/Groceries.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.Class
+{
+    /// <summary>
+    /// Klasse für DataGrid
+    /// </summary>
+    public class Groceries
+    {
+        public int Uid { get; set; }
+        public string Name { get; set; }
+        public double Calories { get; set; }
+        public double Protein { get; set; }
+        public double Carbs { get; set; }
+        public double Fats { get; set; }
+    }
+}
